9fca90f09599d38412b137b0be7c164116691d90
Okan <ozbekk.okan@gmail.com>
2022-06-18 22:59:36 +0300

    updated filtered todo

--- src/components/ToDo/List/Item/index.js
+++ src/components/ToDo/List/Item/index.js
@@ -18,32 +18,34 @@ const Item = ({ todo, todos, setTodos }) => {
   };
 
   return (
-    <label
-      htmlFor={`todo-${todo.id}`}
-      className={`todo__item-container ${todo.completed ? "completed" : ""}`}
-    >
-      <div>
-        <input
-          type="checkbox"
-          name="status"
-          id={`todo-${todo.id}`}
-          className={`${todo.completed ? "completed" : ""}`}
-          onChange={handleOnChange}
-          checked={todo.completed}
-        />
-        <span className={`${todo.completed ? "completed" : ""}`}>
-          {todo.text}
-        </span>
-      </div>
-      <button className="trash__button" onClick={handleRemoveOnClick}>
-        <img
-          src="/assets/icons/times-icon.svg"
-          width={20}
-          height={20}
-          alt="remove-todo"
-        />
-      </button>
-    </label>
+    <>
+      <label
+        htmlFor={`todo-${todo.id}`}
+        className={`todo__item-container ${todo.completed ? "completed" : ""}`}
+      >
+        <div>
+          <input
+            type="checkbox"
+            name="status"
+            id={`todo-${todo.id}`}
+            className={`${todo.completed ? "completed" : ""}`}
+            onChange={handleOnChange}
+            checked={todo.completed}
+          />
+          <span className={`${todo.completed ? "completed" : ""}`}>
+            {todo.text}
+          </span>
+        </div>
+        <button className="trash__button" onClick={handleRemoveOnClick}>
+          <img
+            src="/assets/icons/times-icon.svg"
+            width={20}
+            height={20}
+            alt="remove-todo"
+          />
+        </button>
+      </label>
+    </>
   );
 };
 
